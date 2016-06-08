c2593e1af603a1f280d0c874b8801dc02776421b
Daniel Wei <oweiyco@gmail.com>
2016-06-08 10:34:05 +0800

    db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
 var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000));
@@ -25,6 +26,18 @@ app.get('/times', function(request, response) {
       result += i + ' ';
   response.send(result);
 });
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
